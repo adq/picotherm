09451ea12a71171513b998884bc36efdc029bdb8
Andrew de Quincey <adq@lidskialf.net>
2023-10-19 19:53:45 +0100

    get the pio code working

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import machine
 import rp2
 import time
-from lib import manchester_encode, frame_encode
+from lib import manchester_encode, frame_encode, manchester_decode, frame_decode
 
 
 # opentherm tx - transmit pre-manchester-encoded-bits. Automatically sends start and stop bits.
-@rp2.asm_pio(set_init=rp2.PIO.OUT_LOW, out_init=rp2.PIO.OUT_LOW, autopull=True)
+@rp2.asm_pio(set_init=rp2.PIO.OUT_LOW, out_init=rp2.PIO.OUT_LOW, autopull=True, out_shiftdir=rp2.PIO.SHIFT_RIGHT)
 def opentherm_tx():
     # counter for bit writing loop
     set(x, 31)
@@ -35,60 +35,73 @@ def opentherm_tx():
 sm_opentherm_tx = rp2.StateMachine(0, opentherm_tx, freq=4000, set_base=machine.Pin(0), out_base=machine.Pin(0)) # each tick is 250uS
 
 
-# # opentherm rx - receive manchester-encoded-bits, waiting for initial start bit
-# @rp2.asm_pio(autopush=True)
-# def opentherm_rx():
-#     # wait for start bit
-#     wait(1, pins, 0)
-#     wait(0, pins, 0)
-#     jmp("wait_for_bit_currently_0")
+# opentherm rx - receive manchester-encoded-bits, waiting for initial start bit
+@rp2.asm_pio(autopush=True, in_shiftdir=rp2.PIO.SHIFT_RIGHT)
+def opentherm_rx():
+    # wait for start bit
+    wait(1, pin, 0)
+    wait(0, pin, 0)
 
-#     # read the current bit from the GPIO
-#     label("read_next_bit")
-#     in_(pins, 1)
-#     set(x, 12)  # 13 loops, 4 ticks per loop, 80kHz == 650uS
-#     jmp(pins, "wait_for_bit_currently_1")
+    # kick off initial bit read
+    set(x, 12)  # 13 loops, 3 ticks per loop, 60kHz == 650uS
+    jmp("wait_for_bit_currently_0")
 
-#     # wait for a bit change from 0->1 or timeout
-#     label("wait_for_bit_currently_0")
-#     mov(y, invert(pins))
-#     jmp(y_dec, "read_next_bit")
-#     jmp(dec_x, "wait_for_bit_currently_0")
-#     jmp("read_next_bit")
+    # read the current bit from the GPIO
+    label("read_next_bit")
+    in_(pins, 1)
+    set(x, 12)  # 13 loops, 3 ticks per loop, 60kHz == 650uS
+    jmp(pin, "wait_for_bit_currently_1")
 
-#     # wait for a bit change from 1->0 or timeout
-#     label("wait_for_bit_currently_1")
-#     mov(y, pins)
-#     jmp(y_dec, "read_next_bit")
-#     jmp(x_dec, "wait_for_bit_currently_1")
-#     jmp("read_next_bit")
+    # wait for a bit change from 0->1 or timeout
+    label("wait_for_bit_currently_0")
+    nop()
+    jmp(pin, "read_next_bit")
+    jmp(x_dec, "wait_for_bit_currently_0")
+    jmp("read_next_bit")
 
-# sm_opentherm_rx = rp2.StateMachine(1, opentherm_rx, freq=80000, in_base=machine.Pin(1), jmp_pin=machine.Pin(1)) # each tick is 12.5uS
+    # wait for a bit change from 1->0 or timeout
+    label("wait_for_bit_currently_1")
+    jmp(pin, "was_still_1")
+    jmp("read_next_bit")
+    label("was_still_1")
+    jmp(x_dec, "wait_for_bit_currently_1")
+    jmp("read_next_bit")
+
+sm_opentherm_rx = rp2.StateMachine(4, opentherm_rx, freq=60000, in_base=machine.Pin(1), jmp_pin=machine.Pin(1)) # each tick is 17uS
 
 
-# p = machine.Pin(0, mode=machine.Pin.OUT)
-# p.on()
+sm_opentherm_rx.active(1)
+sm_opentherm_tx.active(1)
 
 
 print("HEYY")
 while True:
-    # p.on()
-    # time.sleep(1)
-    # p.off()
-    # time.sleep(1)
-
-    f = frame_encode(0x06, 0xbb, 0xccdd)
+    x = (0x06, 0xbb, 0xccdd)
+    f = frame_encode(*x)
     m = manchester_encode(f)
-    print(f"{f:032b} {m:064b}")
 
+    sm_opentherm_rx.active(0)
+    while sm_opentherm_rx.rx_fifo():
+        sm_opentherm_rx.get()
+    sm_opentherm_rx.restart()
+    sm_opentherm_rx.active(1)
+
+    sm_opentherm_tx.active(0)
+    sm_opentherm_tx.put(m)
+    sm_opentherm_tx.put(m >> 32)
     sm_opentherm_tx.restart()
     sm_opentherm_tx.active(1)
-    sm_opentherm_tx.put(0)
-    sm_opentherm_tx.put(0)
-    # sm_opentherm_tx.put(m & 0xffffffff)
-    # sm_opentherm_tx.put(m >> 32)
-    print("sleep")
-    time.sleep(1)
+
+    m2 = sm_opentherm_rx.get() | (sm_opentherm_rx.get() << 32)
+
+    f2 = manchester_decode(m2)
+    x2 = frame_decode(f2)
+
+    assert x == x2
+
+    # print(bin(sm_opentherm_rx.get())) # , hex(sm_opentherm_rx.get()))
+    # print("sleep")
+    # time.sleep(1)
 
     # time.sleep(5)
 
