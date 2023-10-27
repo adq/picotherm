47f18a416397674e76943da9b30d373df9891c48
Andrew de Quincey <adq@lidskialf.net>
2023-10-27 09:18:17 +0100

    branch out tests

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import opentherm_app
+import time
 
-while True:
-    try:
-        print(opentherm_app.read_secondary_configuration())
-    except Exception as ex:
-        print(ex)
-    input("Press a key")
+
+def test():
+    while True:
+        try:
+            print(opentherm_app.read_secondary_configuration())
+        except Exception as ex:
+            print(ex)
+        time.sleep(1)
+        input("Press a key")
+
+
+def test2():
+    while True:
+        try:
+            print(opentherm_app.read_secondary_configuration())
+        except Exception as ex:
+            print(ex)
+        input("Press a key")
+
